48002c61e1d8ed4a34c98f2eed37fa7191bddd9a
abhatwar <108324438+abhatwar@users.noreply.github.com>
2022-08-30 22:49:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function test(){
+    console.log("clicked");
+
+    var a=document.getElementById("billamt").value;
+    var b=document.getElementById("peopleamt").value;
+    var e=document.getElementById("option").value;
+
+if(a=='' || b==''|| c=='' ){
+    alert("you have to put the the amount");
+}
+
+    var d=a*e;
+    console.log(d);
+
+   var c = parseInt(d)+parseInt(a);
+   console.log(c);
+
+   var t=c/b;
+
+   var f=c-a;
+   console.log(f);
+
+   var g=f/2;
+   console.log(g);
+
+
+
+   var k=document.createElement('div')
+
+   k.innerHTML='Total amount of bill ='+ c + ', Bill amount per person ='+ t + ',tip per person is = '+g;
+   
+   let o=document.getElementById("tip");
+   o.appendChild(k);
+
+   clearText()
+
+}
+function clearText(){
+document.getElementById("billamt").value='';
+document.getElementById("peopleamt").value='';
+
+}
+
+
+
+
+  
