a892f0c9b83b311201e29f9212fe81ccb016b23f
abhatwar <108324438+abhatwar@users.noreply.github.com>
2022-11-24 23:29:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
-function test(){
+function test() {
     console.log("clicked");
 
-    var a=document.getElementById("billamt").value;
-    var b=document.getElementById("peopleamt").value;
-    var e=document.getElementById("option").value;
+    var BillAmount = document.getElementById("billamt").value;
 
-if(a=='' || b==''|| c=='' ){
-    alert("you have to put the the amount");
+    var Peoples = document.getElementById("peopleamt").value;
+    
+    var Options = document.getElementById("option").value;
+
+    if (BillAmount == '' || Peoples == '') {
+
+        alert("you have to put the amount");
+        return;
+
+    }
+
+    var TotalTax = BillAmount * Options;
+
+    console.log(TotalTax);
+
+    var TotalAmount = parseInt(TotalTax) + parseInt(BillAmount);
+
+    console.log(TotalAmount);
+
+    var PerCandidateamt = TotalAmount / Peoples;
+
+    var PerCandidateTax = TotalTax / Peoples;
+    PerCandidateTax=PerCandidateTax.toFixed(2);
+
+    console.log(PerCandidateTax);
+
+
+    tn=document.getElementById("tip");
+
+
+    var element1 = document.createElement('div')
+    element1.innerHTML = 'Total amount of bill =' + TotalAmount + ', Bill amount per person =' + PerCandidateamt + ',tip per person is = ' + PerCandidateTax;
+
+    tn.appendChild(element1);
+    
+    
+    clearText();
+
+
+function clearText() {
+    document.getElementById("billamt").value = '';
+    document.getElementById("peopleamt").value = '';
+
+
 }
-
-    var d=a*e;//total tip
-    console.log(d);
-
-   var c = parseInt(d)+parseInt(a);//total bill with tip
-   console.log(c);
-
-   var t=c/b;  //total bill distribution per candidate
-
-   var f=c-a;     //tip only=total bill-billamount
-   console.log(f);
-
-   var g=f/b;  //tip distribution
-   console.log(g);
-
-
-
-   var k=document.createElement('div')
-
-   k.innerHTML='Total amount of bill ='+ c + ', Bill amount per person ='+ t + ',tip per person is = '+g;
-   
-   let o=document.getElementById("tip");
-   o.appendChild(k);
-
-   clearText()
-
-}
-function clearText(){
-document.getElementById("billamt").value='';
-document.getElementById("peopleamt").value='';
-
 }
 
 
